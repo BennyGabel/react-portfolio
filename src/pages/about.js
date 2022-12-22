617e50070f0a3247db1b2cfdce7fc24d5f3f5059
13 Miles <bennygabel@gmail.com>
2022-12-21 21:13:54 -0500

    Starting layout in about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,32 @@
 import React, { Component }  from 'react';
-import profile from './BennyGabel'
+import '../index.css';
+import profile from '../img/bennygabel.jpg'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 // Create function
 const About = () => {
     return (
         <div>
-            <img src={profile} alt="Benny Gabel"/>
+            <div>
+                <img src={profile} alt="Benny Gabel" style={{ width: '400px', height: '400px'}}/> 
+            </div>
+            <div id="subtitle">
+                About Me
+            </div>
+
+            <div id="content">
+                {/* Hi!  <br>
+My name is Benny Gabel */}
+            
+            Hi!  {'/n'}
+My name is Benny Gabel 
+
+
+            </div>
         </div>
+
+
     )
 }
 
